2edeefbe1c922564b4af49d27ee6835d5041c3b8
IlVice26 <eliavicentini26@gmail.com>
2023-03-07 15:59:37 +0100

    Changed from composer-plugin to library

--- src/BackupDB.php
+++ src/BackupDB.php
@@ -4,21 +4,23 @@ namespace EuroplanSpa\LaravelDbBackup;
 
 class BackupDB
 {
-	public static function backup($host, $username, $password, $dbname, $bk_path, $bk_filename, $mysqldump = 'mysqldump'){    
+
+  public static function backup($host, $username, $password, $dbname, $bk_path, $bk_filename, $mysqldump = 'mysqldump')
+  {
     // Create backup folder and set permission if not exist.
-		if(!file_exists($bk_path)) {
-			mkdir($bk_path, 0755, true);
-		}
+    if (!file_exists($bk_path)) {
+      mkdir($bk_path, 0755, true);
+    }
 
-		$command = $mysqldump." --user=" . $username ." --password=" . $password . " --host=" . $host . " " . $dbname . " --no-tablespaces  > " . $bk_path . $bk_filename;
-		$output = NULL;
-		$result = NULL;
+    $command = $mysqldump . " --user=" . $username . " --password=" . $password . " --host=" . $host . " " . $dbname . " --no-tablespaces  > " . $bk_path . $bk_filename;
+    $output = NULL;
+    $result = NULL;
 
-		exec($command, $output, $result);
+    exec($command, $output, $result);
 
-		return [
+    return [
       "result" => $result,
       "output" => $output
     ];
-	}
+  }
 }
